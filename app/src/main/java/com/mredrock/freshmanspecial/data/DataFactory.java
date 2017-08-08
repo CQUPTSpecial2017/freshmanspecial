656b6ae5224e7c15478034ce28d362c82a84d81d
nick <948601678@qq.com>
2017-08-08 20:57:23 +0800

    HttpRequest

---
+++ app/src/main/java/com/mredrock/freshmanspecial/data/DataFactory.java
@@ -0,0 +1,50 @@
+package com.mredrock.freshmanspecial.data;
+
+import com.mredrock.freshmanspecial.httptools.GetDataFromServer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.Subscriber;
+
+/**
+ * Created by 700-15isk on 2017/8/8.
+ */
+
+public class DataFactory {
+    private   List<SexRatio>sexRatios;
+    private Subscriber subscriber;
+
+    public List<SexRatio> getSexRatios() {
+
+
+        return sexRatios;
+    }
+
+    public void setSexRatios(List<SexRatio> sexRatios) {
+        this.sexRatios = sexRatios;
+    }
+
+
+    public List<SexRatio> postSexRatio(){
+        sexRatios=new ArrayList<>();
+        subscriber=new Subscriber<List<SexRatio>>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                    e.printStackTrace();
+            }
+
+            @Override
+            public void onNext(List<SexRatio> sexRatio) {
+               sexRatios.addAll(sexRatio);
+            }
+        };
+        GetDataFromServer.getInstance().getSexRatio(subscriber,"SexRatio");
+        return  sexRatios;
+    }
+}
