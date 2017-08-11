61ef7807bbc19c48c73f2dd05cf5c905e0acff7a
Hgsil <az401366794@qq.com>
2017-08-11 22:56:11 +0800

    改滑动

--- app/src/main/java/com/mredrock/freshmanspecial/Ui/View/Special_2017_NoScrollViewPager.java
+++ app/src/main/java/com/mredrock/freshmanspecial/Ui/View/Special_2017_NoScrollViewPager.java
@@ -1,19 +1,23 @@
 package com.mredrock.freshmanspecial.Ui.View;
 
 import android.content.Context;
+import android.support.v4.view.VelocityTrackerCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 
 /**
  * Created by Administrator on 2017/8/11 0011.
  */
 
 public class Special_2017_NoScrollViewPager extends ViewPager {
-    private boolean noScroll = false;
-    private boolean isFirst = true;
+    private boolean noScroll = true;
     private float downX ;    //按下时 的X坐标
     private float downY ;    //按下时 的Y坐标
+    VelocityTracker mVelocityTracker = VelocityTracker.obtain();
+    private static final String TAG = "Special_2017_NoScrollVi";
     public Special_2017_NoScrollViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
         // TODO Auto-generated constructor stub
@@ -38,65 +42,58 @@ public class Special_2017_NoScrollViewPager extends ViewPager {
         //在触发时回去到起始坐标
         float x= ev.getX();
         float y = ev.getY();
-
+        mVelocityTracker.addMovement(ev);
+        mVelocityTracker.computeCurrentVelocity(1000);
         switch (ev.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                //将按下时的坐标存储
-                downX = x;
-                downY = y;
-
-                break;
-            case MotionEvent.ACTION_UP:
-                //获取到距离差
-                float dx= x-downX;
-                float dy = y-downY;
-
-                    orientation = getOrientation(dx, dy);
-
-                break;
+//            case MotionEvent.ACTION_DOWN:
+//                //将按下时的坐标存储
+//                downX = x;
+//                downY = y;
+//
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                //获取到距离差
+//                float dx= x-downX;
+//                float dy = y-downY;
+//
+//                    orientation = getOrientation(dx, dy);
+//
+//                break;
             case MotionEvent.ACTION_MOVE:
-                if (isFirst){
-                    isFirst = false;
-                    return false;
-                }
 
-                else
-                    return super.onTouchEvent(ev);
-
-        }
-        if (orientation == 'x' ) {
-            noScroll = true;
-            return  false;
+                break;
         }
 
-        else
-        return super.dispatchTouchEvent(ev);
-    }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent arg0) {
+            Log.d(TAG, "dispatchTouchEvent: "+(Math.abs(mVelocityTracker.getXVelocity()) >Math.abs(mVelocityTracker.getYVelocity())));
+            Log.d(TAG, "dispatchTouchEvent: "+Math.abs(mVelocityTracker.getXVelocity()) +"Y:"+Math.abs(mVelocityTracker.getYVelocity()));
+            if(Math.abs(mVelocityTracker.getXVelocity())>Math.abs(mVelocityTracker.getYVelocity())) {
+                Log.d(TAG, "dispatchTouchEvent: ");
 
-
-
-        /* return false;//super.onTouchEvent(arg0); */
-
-        if (noScroll){
-            noScroll = false;
-            return false;
-
-        }
-        else return super.onTouchEvent(arg0);
-
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent arg0) {
-            if (noScroll){
-                return true;
+                return false;
             }
-            else
-            return super.onInterceptTouchEvent(arg0);
+
+        return false;
+
     }
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent arg0) {
+//
+//
+//
+//        /* return false;//super.onTouchEvent(arg0); */
+//
+//        if (noScroll){
+//
+//            return false;
+//
+//        }
+//        else return super.onTouchEvent(arg0);
+//
+//    }
+
+
 
     @Override
     public void setCurrentItem(int item, boolean smoothScroll) {
